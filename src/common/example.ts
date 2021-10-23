c82b924c26fa5a2392b1143261c258ccc4b1bb36
Chris Tamez <ctamez232@gmail.com>
2021-10-23 17:09:35 -0500

    example payload for testing

---
+++ src/common/example.ts
@@ -0,0 +1,46 @@
+const exampleRecipe = {
+  "name": "Street Tacos",
+  "type": "DINNER",
+  "images": [],
+  "linkToWebsite": "https://cookingwithcocktailrings.com/2016-mexican-chicken-street-tacos/",
+  "ingredients": [
+    {
+        "name": "Chicken",
+        "measurement": 1,
+        "units": "POUND"
+    },
+    {
+        "name": "Tortillas",
+        "measurement": 3,
+        "units": "NONE"
+    },
+    {
+        "name": "Onion",
+        "measurement": 0.5,
+        "units": "NONE"
+    }
+  ],
+  "steps": [
+    {
+        "stepNumber": 1,
+        "description": "Cook chicken",
+        "time": 20,
+        "timeUnit": "MINUTES",
+        "stepType": "BOIL"
+    },
+    {
+        "stepNumber": 2,
+        "description": "Shred chicken",
+        "time": 5,
+        "timeUnit": "MINUTES",
+        "stepType": "SHRED"
+    }
+  ],
+  "difficulty": "MEDIUM",
+  "keywords": [
+    "tacos",
+    "mexican",
+    "chicken"
+  ],
+  "favorited": true
+};
