8a896a64d18223acdc7c34cb1eae4ef2ba034193
Chris Tamez <ctamez232@gmail.com>
2021-10-22 19:21:22 -0500

    added socket server structure, add functions next

--- src/communications/index.ts
+++ src/communications/index.ts
@@ -1,4 +1,18 @@
 import { Server, Socket } from "socket.io";
+import chalk from 'chalk';
+import { address } from 'ip';
+import {
+  ADD_RECIPE_REQUEST,
+  ADD_RECIPE_RESPONSE,
+  EDIT_RECIPE_REQUEST,
+  EDIT_RECIPE_RESPONSE,
+  DELETE_RECIPE_REQUEST,
+  DELETE_RECIPE_RESPONSE,
+  GET_RECIPES_REQUEST,
+  GET_RECIPES_RESPONSE,
+  GET_RECIPE_BY_ID_REQUEST,
+  GET_RECIPE_BY_ID_RESPONSE
+} from '../communications';
 
 export class Communications {
   public io: Server;
@@ -7,12 +21,48 @@ export class Communications {
   constructor(port: number) {
     this.io = new Server(port);
 
-    console.info(`Opened server at: ${port}`);
+    console.info(chalk.yellow(`Opened server at: localhost:${port} | ${address()}:${port}`));
 
     this.io.on('connect', (socket) => {
-      console.info(`Connected ${socket.id}`);
+      console.info(chalk.cyan(`Connected ${socket.id}`));
+      socket
+        .on(ADD_RECIPE_REQUEST, () => {
+          console.info(chalk.green('Add recipe request...'))
+          // add recipe
+          // emit response
+          // console.info(chalk.green(`Added recipe ${request.name}!`))
+          // socket.emit(ADD_RECIPE_RESPONSE, newRecipe);
+        })
+        .on(EDIT_RECIPE_REQUEST, () => {
+          console.info(chalk.green('Edit recipe request...'))
+          // edit recipe
+          // emit response
+          // console.info(chalk.green(`Edited recipe ${request.name}!`))
+          // socket.emit(EDIT_RECIPE_RESPONSE, modifiedRecipe);
+        })
+        .on(DELETE_RECIPE_REQUEST, () => {
+            console.info(chalk.green('Delete recipe request...'));
+          // delete recipe, only by id
+          // emit response
+          // console.info(chalk.green(`Deleted recipe ${request.name}!`))
+          // socket.emit(DELETE_RECIPE_RESPONSE, deletedRecipe);
+        })
+        .on(GET_RECIPES_REQUEST, () => {
+          console.info(chalk.green('Get recipe request...'));
+          // get all recipes
+          // emit response
+          // console.info(chalk.green('Got all recipes!'));
+          // socket.emit(GET_RECIPES_RESPONSE, allRecipes);
+        })
+        .on(GET_RECIPE_BY_ID_REQUEST, () => {
+          console.info(chalk.green('Get recipe by Id request...'));
+          // get recipe by id
+          // emit response
+          // console.info(chalk.green(`Got recipe ${recipe.name}!`));
+          // socket.emit(GET_RECIPE_BY_ID_RESPONSE, recipeById);
+        });
       socket.on('disconnect', () => {
-        console.info(`Disconnected ${socket.id}`);
+        console.info(chalk.cyan(`Disconnected ${socket.id}`));
       })
       this.socket = socket;
     });
@@ -20,3 +70,16 @@ export class Communications {
 }
 
 export default Communications;
+
+export {
+  ADD_RECIPE_REQUEST,
+  ADD_RECIPE_RESPONSE,
+  EDIT_RECIPE_REQUEST,
+  EDIT_RECIPE_RESPONSE,
+  DELETE_RECIPE_REQUEST,
+  DELETE_RECIPE_RESPONSE,
+  GET_RECIPES_REQUEST,
+  GET_RECIPES_RESPONSE,
+  GET_RECIPE_BY_ID_REQUEST,
+  GET_RECIPE_BY_ID_RESPONSE
+} from './constants';
