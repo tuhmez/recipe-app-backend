c9ef128425448d3aac4668f5aa16a1cc508ace23
Chris Tamez <ctamez232@gmail.com>
2021-10-26 08:10:42 -0500

    changed jest config to typescript friendly file

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from '@jest/types';
+
+export default async (): Promise<Config.InitialOptions> => {
+  return {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    collectCoverageFrom: [
+      "src/**/*.ts*"
+    ],
+    coveragePathIgnorePatterns: [
+      "src/database/"
+    ]
+  }
+};
