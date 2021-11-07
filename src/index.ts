b610c88870336c6841f5fb5022de0c92f39c720b
Chris Tamez <ctamez232@gmail.com>
2021-11-07 14:01:46 -0600

    added server kill catch

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ process.on('SIGINT', function () {
   console.log(chalk.red('Caught interrupt signal, shutting down...'));
   if (server) {
     if (server.socket) server.socket.disconnect();
+    if (server.server) server.server.close();
     server.io.close();
   }
   disconnect();
