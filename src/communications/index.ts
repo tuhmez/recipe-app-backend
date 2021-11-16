80a09b41abb36584431185119993e475775c1e96
Chris Tamez <ctamez232@gmail.com>
2021-11-16 16:10:24 -0600

    removed error endpoint, adapted for responses with error properties

--- src/communications/index.ts
+++ src/communications/index.ts
@@ -14,24 +14,22 @@ import {
   GET_RECIPES_REQUEST,
   GET_RECIPES_RESPONSE,
   GET_RECIPE_BY_ID_REQUEST,
-  GET_RECIPE_BY_ID_RESPONSE,
-  ERROR,
   PING,
   PONG,
 } from './constants';
+import {
+  IAddRecipeRequest,
+  IAddRecipeResponse,
+  IEditRecipeRequest,
+  IEditRecipeResponse,
+  IDeleteRecipeRequest,
+  IDeleteRecipeResponse,
+  IGetRecipeByIdRequest,
+  IGetRecipesResponse,
+} from './interfaces';
 import { IRecipe } from "../common/types";
 import { RecipeModel } from "../database";
 import { databaseDocumentToRecipe } from "../utils/converters";
-import { errorMessageCreator } from "../utils/error";
-
-interface IRecipeByIdRequest {
-  recipeId: string;
-}
-
-interface IUpdateRecipeRequest {
-  recipeId: string;
-  recipe: IRecipe;
-}
 
 export class Communications {
   public server: Server;
@@ -41,90 +39,123 @@ export class Communications {
   constructor(port: number) {
     const app = express();
     this.server = createServer(app);
-    this.io = new SocketServer(this.server, { cors: { origin: '*' }});
+    this.io = new SocketServer(this.server, { cors: { origin: '*' } });
 
     this.io.on('connect', (socket) => {
       console.info(chalk.cyan(`Connected ${socket.id}`));
       socket
-      .on(PING, () => {
-        socket.emit(PONG, { status: 'OK' });
-      })
-      .on(ADD_RECIPE_REQUEST, (request: IRecipe) => {
-        if (!request) {
-          const err = 'Invalid recipe request!';
-          console.info(chalk.red(err));
-          socket.emit(ERROR, errorMessageCreator(err));
-          return;
-        }
-        console.info(chalk.green('Add recipe request...'));
-        RecipeModel.find({}, (err, docs) => {
-          if (err) {
-            console.info(chalk.red(err));
-            socket.emit(ERROR, errorMessageCreator(JSON.stringify(err)));
+        .on(PING, () => {
+          socket.emit(PONG, { status: 'OK' });
+        })
+        .on(ADD_RECIPE_REQUEST, (request: IAddRecipeRequest) => {
+          if (!request) {
+            const error = 'Invalid recipe request!';
+            console.info(chalk.red(error));
+            const res: IAddRecipeResponse = {
+              error
+            };
+            socket.emit(ADD_RECIPE_RESPONSE, res);
             return;
           }
-          request.recipeId = uuidv4();
-          new RecipeModel(request)
-          .save()
-          .then(
-            (doc) => {
-              const newRecipe = databaseDocumentToRecipe(doc, false);
-              console.info(chalk.green(`Added recipe ${newRecipe.name}!`));
-              socket.emit(ADD_RECIPE_RESPONSE, newRecipe);
-            },
-            (err) => {
+          console.info(chalk.green('Add recipe request...'));
+          RecipeModel.find({}, (err, docs) => {
+            if (err) {
               console.info(chalk.red(err));
-              socket.emit(ERROR, errorMessageCreator(JSON.stringify(err)));
-            }
-            );
-          });
-        })
-          .on(EDIT_RECIPE_REQUEST, (request: IUpdateRecipeRequest) => {
-            const { recipeId, recipe } = request;
-            console.info(chalk.green('Edit recipe request...'));
-            RecipeModel.findOneAndUpdate({ recipeId }, recipe, { new: true }, (err, doc) => {
-              if (err) {
-                console.info(chalk.red(err));
-                socket.emit(ERROR, errorMessageCreator(JSON.stringify(err)));
-                return;
+              const res: IAddRecipeResponse = {
+                error: JSON.stringify(err)
               }
-              if (!doc) {
-                const error = `Couldn\'t find recipe of Id: ${recipeId}`;
-                console.info(chalk.red(error));
-                socket.emit(ERROR, errorMessageCreator(error));
-                return;
-              }
-              const editedRecipe = databaseDocumentToRecipe(doc, false);
-              console.info(chalk.green(`Edited recipe ${editedRecipe.name}!`));
-              socket.emit(EDIT_RECIPE_RESPONSE, editedRecipe);
-            });
-          })
-          .on(DELETE_RECIPE_REQUEST, (request: IRecipeByIdRequest) => {
-            console.info(chalk.green('Delete recipe request...'));
-            if (!request || !request.recipeId) {
-              const err = 'Invalid Id for request';
-              console.info(chalk.red(err));
-              socket.emit(ERROR, errorMessageCreator(err));
+              socket.emit(ADD_RECIPE_RESPONSE, res);
               return;
             }
-            console.info(chalk.green('Delete recipe by Id request...'));
-            const { recipeId } = request;
-            RecipeModel.findOneAndDelete({ recipeId }, null, (err, doc) => {
-              if (err) {
-                console.info(chalk.red(err));
-                socket.emit(ERROR, errorMessageCreator(JSON.stringify(err)));
-                return;
+            request.recipeId = uuidv4();
+            new RecipeModel(request)
+              .save()
+              .then(
+                (doc) => {
+                  const newRecipe = databaseDocumentToRecipe(doc, false);
+                  console.info(chalk.green(`Added recipe ${newRecipe.name}!`));
+                  const res: IAddRecipeResponse = {
+                    recipe: newRecipe
+                  };
+                  socket.emit(ADD_RECIPE_RESPONSE, res);
+                },
+                (err) => {
+                  console.info(chalk.red(err));
+                  const res: IAddRecipeResponse = {
+                    error: JSON.stringify(err)
+                  };
+                  socket.emit(ADD_RECIPE_RESPONSE, res);
+                }
+              );
+          });
+        })
+        .on(EDIT_RECIPE_REQUEST, (request: IEditRecipeRequest) => {
+          const { recipeId } = request;
+          console.info(chalk.green('Edit recipe request...'));
+          RecipeModel.findOneAndUpdate({ recipeId }, request, { new: true }, (err, doc) => {
+            if (err) {
+              console.info(chalk.red(err));
+              const res: IEditRecipeResponse = {
+                error: JSON.stringify(err)
+              };
+              socket.emit(EDIT_RECIPE_RESPONSE, res);
+              return;
+            }
+            if (!doc) {
+              const error = `Couldn\'t find recipe of Id: ${recipeId}`;
+              console.info(chalk.red(error));
+              const res: IEditRecipeResponse = {
+                error: JSON.stringify(error)
               }
-              if (!doc) {
-                const error = `Couldn\'t find recipe of Id: ${recipeId}`;
-                console.info(chalk.red(error));
-                socket.emit(ERROR, errorMessageCreator(error));
-                return;
-              }
-              const deletedRecipe = databaseDocumentToRecipe(doc, false);
-              console.info(chalk.green(`Deleted recipe ${deletedRecipe.name}!`));
-              socket.emit(DELETE_RECIPE_RESPONSE, deletedRecipe);
-            });
+              socket.emit(EDIT_RECIPE_RESPONSE, res);
+              return;
+            }
+            const editedRecipe = databaseDocumentToRecipe(doc, false);
+            console.info(chalk.green(`Edited recipe ${editedRecipe.name}!`));
+            const res: IEditRecipeResponse = {
+              recipe: editedRecipe
+            };
+            socket.emit(EDIT_RECIPE_RESPONSE, res);
+          });
+        })
+        .on(DELETE_RECIPE_REQUEST, (request: IDeleteRecipeRequest) => {
+          console.info(chalk.green('Delete recipe request...'));
+          if (!request || !request.recipeId) {
+            const error = 'Invalid Id for request';
+            console.info(chalk.red(error));
+            const res: IDeleteRecipeResponse = {
+              error
+            };
+            socket.emit(DELETE_RECIPE_RESPONSE, res);
+            return;
+          }
+          console.info(chalk.green('Delete recipe by Id request...'));
+          const { recipeId } = request;
+          RecipeModel.findOneAndDelete({ recipeId }, null, (err, doc) => {
+            if (err) {
+              console.info(chalk.red(err));
+              const res: IDeleteRecipeResponse = {
+                error: JSON.stringify(err)
+              };
+              socket.emit(DELETE_RECIPE_RESPONSE, res);
+              return;
+            }
+            if (!doc) {
+              const error = `Couldn\'t find recipe of Id: ${recipeId}`;
+              console.info(chalk.red(error));
+              const res: IDeleteRecipeResponse = {
+                error
+              };
+              socket.emit(DELETE_RECIPE_RESPONSE, res);
+              return;
+            }
+            const deletedRecipe = databaseDocumentToRecipe(doc, false);
+            console.info(chalk.green(`Deleted recipe ${deletedRecipe.name}!`));
+            const res: IDeleteRecipeResponse = {
+              recipe: deletedRecipe
+            };
+            socket.emit(DELETE_RECIPE_RESPONSE, res);
+          });
         })
         .on(GET_RECIPES_REQUEST, () => {
           console.info(chalk.green('Get recipe request...'));
@@ -132,19 +163,28 @@ export class Communications {
           RecipeModel.find({}, (err, docs) => {
             if (err) {
               console.info(chalk.red(err));
-              socket.emit(ERROR, errorMessageCreator(JSON.stringify(err)));
+              const res: IGetRecipesResponse = {
+                error: JSON.stringify(err)
+              };
+              socket.emit(GET_RECIPES_RESPONSE, res);
               return;
             }
             allRecipes = docs.map((d, i) => databaseDocumentToRecipe(d, false));
             console.info(chalk.green('Got all recipes!'));
-            socket.emit(GET_RECIPES_RESPONSE, allRecipes);
+            const res: IGetRecipesResponse = {
+              recipes: allRecipes
+            };
+            socket.emit(GET_RECIPES_RESPONSE, res);
           });
         })
-        .on(GET_RECIPE_BY_ID_REQUEST, (request: IRecipeByIdRequest) => {
+        .on(GET_RECIPE_BY_ID_REQUEST, (request: IGetRecipeByIdRequest) => {
           if (!request || !request.recipeId) {
-            const err = 'Invalid Id for request!';
-            console.info(chalk.red(err));
-            socket.emit(ERROR, errorMessageCreator(err));
+            const error = 'Invalid Id for request!';
+            console.info(chalk.red(error));
+            const res: IGetRecipesResponse = {
+              error
+            };
+            socket.emit(GET_RECIPES_RESPONSE, res);
             return;
           }
           console.info(chalk.green('Get recipe by Id request...'));
@@ -152,29 +192,38 @@ export class Communications {
           RecipeModel.findOne({ recipeId }, null, null, (err, doc) => {
             if (err) {
               console.info(chalk.red(err));
-              socket.emit(ERROR, errorMessageCreator(JSON.stringify(err)));
+              const res: IGetRecipesResponse = {
+                error: JSON.stringify(err)
+              };
+              socket.emit(GET_RECIPES_RESPONSE, res);
               return;
             }
             if (!doc) {
               const error = `Couldn\'t find recipe of Id: ${recipeId}`;
               console.info(chalk.red(error));
-              socket.emit(ERROR, errorMessageCreator(error));
+              const res: IGetRecipesResponse = {
+                error
+              };
+              socket.emit(GET_RECIPES_RESPONSE, res);
               return;
             }
             const recipeById = databaseDocumentToRecipe(doc, false);
             console.info(chalk.green(`Got recipe ${recipeById.name}!`));
-            socket.emit(GET_RECIPE_BY_ID_RESPONSE, recipeById);
+            const res: IGetRecipesResponse = {
+              recipes: [recipeById]
+            };
+            socket.emit(GET_RECIPES_RESPONSE, res);
           });
         });
-        socket.on('disconnect', () => {
-          console.info(chalk.cyan(`Disconnected ${socket.id}`));
-        })
-        this.socket = socket;
-      });
-      this.server.listen(port, () => {
-        console.info(chalk.yellow(`Opened server at: localhost:${port} | ${address()}:${port}`));
-      });
-    }
+      socket.on('disconnect', () => {
+        console.info(chalk.cyan(`Disconnected ${socket.id}`));
+      })
+      this.socket = socket;
+    });
+    this.server.listen(port, () => {
+      console.info(chalk.yellow(`Opened server at: localhost:${port} | ${address()}:${port}`));
+    });
+  }
 }
 
 export default Communications;
---
+++ src/communications/interfaces.ts
@@ -0,0 +1,34 @@
+import { IRecipe } from '../common/types';
+
+// Add Recipe Interfaces
+export interface IAddRecipeRequest extends IRecipe {}
+export interface IAddRecipeResponse {
+  recipe?: IRecipe;
+  error?: string;
+}
+
+// Edit Recipe Interfaces
+export interface IEditRecipeRequest extends IRecipe {}
+export interface IEditRecipeResponse {
+  recipe?: IRecipe;
+  error?: string;
+}
+
+// Delete Recipe Interfaces
+export interface IDeleteRecipeRequest {
+  recipeId: string;
+}
+export interface IDeleteRecipeResponse {
+  recipe?: IRecipe;
+  error?: string;
+}
+
+// Get Recipe Interfaces
+export interface IGetRecipeByIdRequest {
+  recipeId: string;
+}
+export interface IGetRecipesResponse {
+  recipeId?: string;
+  recipes?: IRecipe[];
+  error?: string;
+}
